02ee3b638813fe8790d8fe3c6cdf490cbc3c0609
uploadgithub <91936976+uploadgithub@users.noreply.github.com>
2021-10-05 15:17:43 +0900

    commit

---
+++ jungol/src/jungol/Main.java
@@ -0,0 +1,40 @@
+package jungol;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		
+		Double a = 0.0;
+		
+		a = sc.nextDouble();
+		
+		if(a<=50.80)
+		{
+			System.out.println("Flyweight");
+		}
+		else if(a<=61.23)
+		{
+			System.out.println("Lightweight");
+		}
+		else if(a<=72.57)
+		{
+			System.out.println("Middleweight");
+		}
+		else if(a<=88.45)
+		{
+			System.out.println("Cruiserweight");
+		}
+		else if(a>88.45)
+		{
+			System.out.println("Heavyweight");
+		}
+		
+		sc.close();
+	}
+
+}
